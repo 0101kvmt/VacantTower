d34527f1e3127b4d9a36f1f4ae3657b6dfeb0986
0101kvmt <0101kvmt@gmail.com>
2018-02-09 12:17:00 -0800

    Player movement

---
+++ Vacant Tower/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float playerSpeed;
+
+    public Vector2 playerLastMovement;
+
+    private bool playerMovement;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerMovement = false;
+
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * playerSpeed * Time.deltaTime, 0f, 0f));
+            playerMovement = true;
+            playerLastMovement = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * playerSpeed * Time.deltaTime, 0f));
+            playerMovement = true;
+            playerLastMovement = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+        }
+
+    }
+}
