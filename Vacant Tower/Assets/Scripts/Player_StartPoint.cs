b55b4f5fb73e07d422eba2a9aace5a27629ea77a
0101kvmt <0101kvmt@gmail.com>
2018-02-11 19:35:45 -0800

    camera controller and startpoint

---
+++ Vacant Tower/Assets/Scripts/Player_StartPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_StartPoint : MonoBehaviour {
+
+    private PlayerMovement thePlayer;
+    private CameraController theCamera;
+
+    public Vector2 startDirection;
+
+    // Use this for initialization
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+        thePlayer.transform.position = transform.position;
+        thePlayer.lastMove = startDirection;
+        theCamera = FindObjectOfType<CameraController>();
+        theCamera.transform.position = new Vector3(transform.position.x, transform.position.y, theCamera.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
